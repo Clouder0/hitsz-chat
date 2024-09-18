83de0f89565d8ef0c37d7316f14fd3094df0034b
Clouder0 <clouder0@outlook.com>
2024-09-18 23:30:22 +0800

    feat: use middleware mode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { type Context, Schema } from "koishi";
+import { type Context, Schema, Session } from "koishi";
 import { API } from "./api";
 import { v4 as uuidv4 } from "uuid";
 
@@ -35,9 +35,32 @@ export function apply(ctx: Context) {
 		last_time: "timestamp",
 		conversation_id: "string",
 	});
+  
+  const handleChat = async(conversation_id: string, user_id: string, query: string) => {
+		try {
+			const res = await api.sendChatMsg(
+				query,
+				user_id,
+				conversation_id,
+			);
+
+			await ctx.database.upsert(db, (_) => [
+				{
+					id: user_id,
+					last_time: new Date(),
+					conversation_id: res.conversation_id,
+				},
+			]);
+      return res.answer;
+		} catch (e) {
+			console.error(e);
+			return "抱歉，出现了未知错误！";
+		}
+  }
 
 	ctx
-		.command("chat end", "停止本轮对话")
+		.command("chat")
+    .subcommand("end", "终止本轮对话")
 		.usage("停止本轮对话，终止当前 Session，重新开始一个新的话题。")
 		.action(async (options) => {
 			const user_id = options.session.author.id;
@@ -45,42 +68,27 @@ export function apply(ctx: Context) {
 			options.session.send("已终止本轮对话。");
 		});
 
-	ctx.on("message", async (session) => {
-		const user_id = session.author.id;
+  ctx.command("ask <msg:text>").action(async(a) => {
+    // always start a new conversation
+    const res = await handleChat("", a.session.author.id, a.session.content);
+    await a.session.send(res);
+  })
+  
+  ctx.middleware(async (session, next) => {
+    const user_id = session.author.id;
 		const prev = await ctx.database.get(db, user_id);
-		console.log("prev", prev);
-
-		let conversation_id = "";
+    let conversation_id = "";
 		const now = Date.now();
 		// 5 minutes refresh conversation session
 		if (prev.length > 0 && prev[0].last_time > new Date(now - 1000 * 60 * 5)) {
 			conversation_id = prev[0].conversation_id;
 		}
-		console.log("conversation_id", conversation_id);
-
-		try {
-			const res = await api.sendChatMsg(
-				session.content,
-				user_id,
-				conversation_id,
-			);
-
-			console.log("res", res);
-			const send_task = session.send(res.answer);
-			const set_task = ctx.database.upsert(db, (_) => [
-				{
-					id: user_id,
-					last_time: new Date(now),
-					conversation_id: res.conversation_id,
-				},
-			]);
-			console.log("set database", new Date(now), user_id, res.conversation_id);
-			await Promise.all([send_task, set_task]);
-			console.log("send done, res", res.answer);
-		} catch (e) {
-			console.error(e);
-			await session.send("抱歉，出现了未知错误！");
-			return;
-		}
-	});
+    if(conversation_id === "") {
+      // in this mode only handles session msg
+      next();
+    }
+    const res = await handleChat(conversation_id, user_id, session.content);
+    await session.send(res);
+   
+  })
 }
